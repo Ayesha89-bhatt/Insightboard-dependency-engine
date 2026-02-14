ef17ef44535ec3046e19bf13ae96942ba03d1ebf
Ayesha89-bhatt <bhatttripti2000@gmail.com>
2026-02-14 12:49:07 +0530

    chnages in service file

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+
 export const generateGraph = async (transcript: string) => {
-  const res = await axios.post("http://localhost:8080/api/transcript", {
+
+  const BASE_URL = import.meta.env.VITE_API_BASE_URL ?? "http://localhost:8080/api/transcript";
+  
+
+  const res = await axios.post(BASE_URL, {
     transcript,
   });
   return res.data;
