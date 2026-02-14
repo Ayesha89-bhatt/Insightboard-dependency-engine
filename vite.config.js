71b2b72806f1e918264c7a69bdf4d4a2673273cb
Ayesha89-bhatt <bhatttripti2000@gmail.com>
2026-02-14 14:05:21 +0530

    changes in config file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  preview: {
+    host: true,          // Bind to 0.0.0.0
+    allowedHosts: 'all'  // Allow all external hosts (Render compatible)
+  }
 })
